6c081cc0b7dc8f4b9d0be50fe7f5fad5bdf44912
sallypikachu <sallypikachu@gmail.com>
2017-07-11 18:33:13 -0400

    Simple login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  root 'home#index'
   get 'home', to: 'home#index'
+  devise_for :users
+  get 'hello_world', to: 'hello_world#index'
 end
