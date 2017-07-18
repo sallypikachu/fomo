4a90a6ac818c2f7ffb12e4ef3aafb6e5177424f6
sallypikachu <sallypikachu@gmail.com>
2017-07-17 22:00:47 -0400

    Test dummy events#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home', to: 'home#index'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do 
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  resources :events, only: :index
   get 'hello_world', to: 'hello_world#index'
 end
