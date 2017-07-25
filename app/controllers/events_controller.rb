069df5655a3767d93225f0ab13801554c07da755
sallypikachu <sallypikachu@gmail.com>
2017-07-25 19:04:07 -0400

    Display of upcoming events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,6 @@
 class EventsController < ApplicationController
 
   def index
-    @events = current_user.access_token ? Koala::Facebook::API.new(current_user.access_token).get_object('/me/events?time_filter=past') : []
-    @events.select! {|x| Date.parse(x['start_time']) > Date.today && Date.parse(x['end_time']) < (Date.today + 60.days) }
+    @events = current_user.access_token ? Koala::Facebook::API.new(current_user.access_token).get_object('/me/events?time_filter=past').select {|x| Date.parse(x['start_time']) > Date.today && Date.parse(x['end_time']) < (Date.today + 60.days) } : []
   end
 end
